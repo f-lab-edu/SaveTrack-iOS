b7d3ae0b3edcef0e550f12fd3557fc18226590e9
z3rosmith <52317025+z3rosmith@users.noreply.github.com>
2024-09-13 01:39:43 +0900

    fix BaseResponseDTO

--- SaveTrack/Sources/DTO/BaseResponseDTO.swift
+++ SaveTrack/Sources/DTO/BaseResponseDTO.swift
@@ -9,27 +9,32 @@ import Foundation
 
 struct BaseResponseDTO<T: Decodable>: Decodable {
     let code: Int
-    let message: String
-    let data: DataContent<T>
+    let message: String?
+    let data: DataContent<T>?
+    
+    init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.code = try container.decode(Int.self, forKey: CodingKeys.code)
+        self.message = try container.decodeIfPresent(String.self, forKey: CodingKeys.message)
+        
+        if let data = try container.decodeIfPresent(T.self, forKey: CodingKeys.data) {
+            self.data = .data(data)
+        } else if let stackTrace = try container.decodeIfPresent([String].self, forKey: CodingKeys.data) {
+            self.data = .stackTrace(stackTrace)
+        } else {
+            throw DecodingError.dataCorruptedError(forKey: .data, in: container, debugDescription: "DataContent does not match any known type")
+        }
+    }
+    
+    enum CodingKeys: CodingKey {
+        case code
+        case message
+        case data
+    }
     
     enum DataContent<U: Decodable>: Decodable {
         case data(U)
         case stackTrace([String])
-        
-        init(from decoder: Decoder) throws {
-            let container = try decoder.container(keyedBy: CodingKeys.self)
-            if let data = try? container.decode(U.self, forKey: .data) {
-                self = .data(data)
-            } else if let stackTrace = try? container.decode([String].self, forKey: .stackTrace) {
-                self = .stackTrace(stackTrace)
-            } else {
-                throw DecodingError.dataCorruptedError(forKey: .data, in: container, debugDescription: "DataContent does not match any known type")
-            }
-        }
-        
-        enum CodingKeys: String, CodingKey {
-            case data
-            case stackTrace
-        }
     }
 }
