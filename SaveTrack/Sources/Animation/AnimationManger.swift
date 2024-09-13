4c852478870d4b750ee02526dff0ffc7ce45e08e
Jang HuiSeon <xkzl8521@gmail.com>
2024-09-13 19:53:01 +0900

    Fix: keywindow 추출 로직 변경

--- SaveTrack/Sources/Animation/AnimationManger.swift
+++ SaveTrack/Sources/Animation/AnimationManger.swift
@@ -35,18 +35,12 @@ final class AnimationManger {
         emitterLayer.emitterSize = CGSize(width: UIScreen.main.bounds.width, height: 1)
         emitterLayer.emitterPosition = CGPoint(x: UIScreen.main.bounds.width / 2, y: 100)
                
-        //cell 의 birth rate와 곱해져서 총 birth rate 가 정해짐
         emitterLayer.birthRate = 1
         
-        //birthRate를 0을 설정해주지 않으면 시간동안 계속 cell을 방출함.
-        // 한번 방출하고 끝내는것 처럼 보이게 엄청 짧게 설정
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.01) { [weak self] in
-            //birth rate 가 0이 되면 더 이상 값을 방출하지 않는 것처럼 보임
             self?.emitterLayer.birthRate = 0
         }
-        // 레이어 얹어주면 방출 시작되는 것 보임.
-        // 신기한건 클릭할때마다 addSublayer가 불리니까 layer가 계속 쌓일거 같은데 count 로 찍어보면 계속 1임
-        UIApplication.firstKeyWindowForConnectedScenes?.window?.layer.addSublayer(emitterLayer)
+        UIApplication.shared.keyWindow?.layer.addSublayer(emitterLayer)
     }
 }
 
